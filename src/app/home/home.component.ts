9d2b532a8e50fba17c8a124959ecf6d657331966
Mayran Adriel <mayranalbuquerque@gmail.com>
2024-02-29 16:26:24 -0300

    adicionando mecanismo de musca no json

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { TitleComponent } from './title/title.component';
 import { QuestionComponent } from './question/question.component';
 import { MainQuestionComponent } from './main-question/main-question.component';
+import { ResultComponent } from './result/result.component';
 import data from '../../assets/data/quiz_questions.json';
 
 @Component({
@@ -15,36 +16,38 @@ import data from '../../assets/data/quiz_questions.json';
     QuestionComponent,
     MainQuestionComponent,
     CommonModule,
+    ResultComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
-
-  listQuestion = data.questions
+  listQuestion = data.questions;
 
   dataQuestions = {
     title: data.title,
-    listQuestion: data.questions
-  }
+    listQuestion: data.questions,
+  };
+
+  limitReached: boolean = false;
 
   index: number = 0;
 
-  maxQuestions = data.questions.length
+  maxQuestions = data.questions.length - 1;
 
-  listOfAnswers: any[] = []
+  listOfAnswers: any[] = [];
 
-  mechanism(response: string){
-    this.listOfAnswers[this.index] = response
+  mechanism(response: string) {
+    this.listOfAnswers[this.index] = response;
 
-
-    this.index += 1;
-    console.log(this.listOfAnswers)
+    if (this.index === this.maxQuestions) {
+      this.limitReached = true;
+    } else {
+      this.index += 1;
+    }
   }
 
   ngOnInit(): void {
-
+    console.log(this.maxQuestions);
   }
-
-  }
-
+}
